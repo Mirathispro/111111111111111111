dbee8db500f2b8ec5fb3f91b9da47f23921a935f
Mirathispro <>
2024-07-04 12:48:09 +0000

    Updates

---
+++ _next/static/WEbEo_tfeHGPWE19BYMB1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/786c17b3dc91478b.css","static/chunks/pages/index-7cb19628d89f01a0.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
